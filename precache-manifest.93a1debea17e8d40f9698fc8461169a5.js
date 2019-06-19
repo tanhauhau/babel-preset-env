51f7b24e09e8e553e7ce54ef962ebf342c8fa4c8
Li Hau Tan <tanlh@seagroup.com>
2019-06-19 12:28:17 +0800

    Built on Wed Jun 19 2019 12:28:17 GMT+0800 (+08)

---
+++ precache-manifest.93a1debea17e8d40f9698fc8461169a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ebf63a06906fd77c039981092baee3",
+    "url": "/babel-preset-env/index.html"
+  },
+  {
+    "revision": "90b05f07c312fbf6d8a0",
+    "url": "/babel-preset-env/static/css/2.59ca6ef4.chunk.css"
+  },
+  {
+    "revision": "c3502b1ea07f873118ff",
+    "url": "/babel-preset-env/static/css/main.6684dbe0.chunk.css"
+  },
+  {
+    "revision": "90b05f07c312fbf6d8a0",
+    "url": "/babel-preset-env/static/js/2.c76e457d.chunk.js"
+  },
+  {
+    "revision": "c3502b1ea07f873118ff",
+    "url": "/babel-preset-env/static/js/main.c1a138fe.chunk.js"
+  },
+  {
+    "revision": "a75cfb4d063bae3142c8",
+    "url": "/babel-preset-env/static/js/runtime~main.87519db0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/babel-preset-env/precache-manifest.cfec57e6170b4ccaa1a2e216feb2c4e9.js"
+  "/babel-preset-env/precache-manifest.93a1debea17e8d40f9698fc8461169a5.js"
 );
 
 self.addEventListener('message', (event) => {
